aeaa909ac5c5e810616dcb4faf64f53ad0d23d9a
Kasper Langer <kasperlanger@me.com>
2015-01-28 18:42:37 +0000

    conditional `apt-get update` and tmux and tig

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-sudo apt-get install zsh
+if [ "$[$(date +%s) - $(stat -c %Z /var/lib/apt/periodic/update-success-stamp)]" -ge 600000 ]; then
+  sudo apt-get update
+fi
+sudo apt-get install zsh tmux tig
 chsh -s /usr/bin/zsh
 
 for file in tmux.conf vimrc gitconfig zshrc; do
