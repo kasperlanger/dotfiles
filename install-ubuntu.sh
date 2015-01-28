08295973ebfbcfd76546b8a5bbf9a311d3154f66
Kasper Langer <kasperlanger@me.com>
2015-01-28 19:15:39 +0000

    add git to ubuntu

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 if [ "$[$(date +%s) - $(stat -c %Z /var/lib/apt/periodic/update-success-stamp)]" -ge 600000 ]; then
   sudo apt-get update
 fi
-sudo apt-get install zsh tmux tig
+sudo apt-get git install zsh tmux tig
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 chsh -s /usr/bin/zsh
 
 for file in tmux.conf vimrc gitconfig zshrc; do
